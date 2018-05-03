da9a962717306f008c8ef1592776740db082de63
Christoph Ulrich <christoph_ulrich@gmx.de>
2018-05-03 18:29:35 +0200

    started backpropagation

--- 02/ue02_logical_gates_bp.py
+++ 02/ue02_logical_gates_bp.py
@@ -9,8 +9,8 @@ def sigmoid_derivative(x):
     return sigmoid(x) * (1.0 - sigmoid(x))
 
 
-def mean_squared_error():
-    pass
+def mean_squared_error(predictions, actual):
+    return 1/predictions.shape[0] * ((actual.T - predictions) ** 2).sum(axis=0)
 
 
 # Trainingsdaten
@@ -18,6 +18,7 @@ training_input = np.array([[0, 0],
                            [0, 1],
                            [1, 0],
                            [1, 1]])
+
 training_output = np.array([[0, 0, 0],
                             [0, 1, 1],
                             [0, 1, 1],
@@ -35,7 +36,28 @@ if __name__ == '__main__':
     hidden_layer = sigmoid(np.dot(weights_h.T, input_set.T) + bias_h.T)
     output_layer = sigmoid(np.dot(weights_o.T, hidden_layer) + bias_o.T)
 
+    for i in range(0, 1000):
+        for j in range(0, training_input.shape[0]):
+            input_set = np.array([training_input[j]])
+            actual = np.array([training_output[j]])
+            hidden_layer = sigmoid(np.dot(weights_h.T, input_set.T) + bias_h.T)
+            predictions = sigmoid(np.dot(weights_o.T, hidden_layer) + bias_o.T)
+
+            mse = mean_squared_error(predictions, actual)
+
+            print('### Schritt ' + str(i+1) + ' ###')
+            print('Vorhersage für ' + str(input_set) + ' --> ' + str(output_layer))
+            print('MSE --> ' + str(mse))
+
+            Delta_L_by_A = 2*predictions - 2*actual
+
+
+
+
+    '''
+    print(training_input.shape)
     print('Input: ' + str(input_set[0]))
     print('NN -> AND: ' + str(output_layer[0][0]))
     print('NN -> OR: ' + str(output_layer[1][0]))
     print('NN -> XOR: ' + str(output_layer[2][0]))
+    '''
