ea3ad283d18a0dd084bdfa3ff50ded160c3f1dd6
Christoph Ulrich <christoph_ulrich@gmx.de>
2018-06-14 18:33:08 +0200

    Added loop to test batch sizes

--- 03/ue03_pytroch_mnist_new.py
+++ 03/ue03_pytroch_mnist_new.py
@@ -1,11 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Konfiguration 
-momentum = 0.9
-learning_rate = 0.001
-mini_batch_size = 10
-optimizer_type = 'sgd'
-
+import torch.nn as nn
+import torch.nn.functional as F
 import numpy as np
 import torch
 import torchvision
@@ -13,48 +9,25 @@ import torchvision.transforms as transforms
 import torchvision.datasets as dset
 from torch.utils.data.sampler import SubsetRandomSampler
 import os
-    
+import matplotlib.pyplot as plt
+import torch.optim as optim
+
+# Konfiguration
+momentum = 0.9
+learning_rate = 0.001
+mini_batch_size = 10
+mini_batch_sizes = [1, 10, 50, 100, 500]
+optimizer_type = 'sgd'
+
 trans = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (1.0,))])
 
-root = './data'
-if not os.path.exists(root):
-    os.mkdir(root)
-train_set = dset.MNIST(root=root, train=True, transform=trans, download=True)
-test_set = dset.MNIST(root=root, train=False, transform=trans, download=True)
-
-trainloader = torch.utils.data.DataLoader(train_set, batch_size=mini_batch_size,
-                                          shuffle=True, num_workers=2)
-
-testloader = torch.utils.data.DataLoader(test_set, batch_size=mini_batch_size,
-                                         shuffle=False, num_workers=2)
-
-classes = np.arange(10) # classes from 0-9
-
-import matplotlib.pyplot as plt
-import numpy as np
 
 # functions to show an image
-
-
 def imshow(img):
     img = img / 2 + 0.5     # unnormalize
     npimg = img.numpy()
     plt.imshow(np.transpose(npimg, (1, 2, 0)))
-    plt.show()
-
-
-# get some random training images
-dataiter = iter(trainloader)
-images, labels = dataiter.next()
-
-# show images
-imshow(torchvision.utils.make_grid(images))
-# print labels
-print(' '.join('%5s' % classes[labels[j]] for j in range(1)))
-
-
-import torch.nn as nn
-import torch.nn.functional as F
+    #plt.show()
 
 
 class Net(nn.Module):
@@ -77,133 +50,118 @@ class Net(nn.Module):
         return x
 
 
-net = Net()
+if __name__ == '__main__':
+    for batch_size in mini_batch_sizes:
+        print("Optimizer {}, batch_size {}, learning_rate {}, momentum {}".format(optimizer_type, batch_size, learning_rate, momentum))
+        root = './data'
+        if not os.path.exists(root):
+            os.mkdir(root)
+        train_set = dset.MNIST(root=root, train=True, transform=trans, download=True)
+        test_set = dset.MNIST(root=root, train=False, transform=trans, download=True)
 
+        trainloader = torch.utils.data.DataLoader(train_set, batch_size=batch_size,
+                                                  shuffle=True, num_workers=2)
 
-import torch.optim as optim
+        testloader = torch.utils.data.DataLoader(test_set, batch_size=batch_size,
+                                                 shuffle=False, num_workers=2)
 
-criterion = nn.CrossEntropyLoss()
+        classes = np.arange(10)  # classes from 0-9
 
-if optimizer_type == 'sgd':
-    optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=momentum)
-elif optimizer_type == 'rms_prop':
-    optimizer = optim.RMSprop(net.parameters(), lr=learning_rate, momentum=momentum)
-elif optimizer_type == 'ada_delta':
-    optimizer = optim.Adadelta(net.parameters(), lr=learning_rate, rho=0.9, eps=1e-06, weight_decay=0)
-else:
-    # default sgd
-    optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=momentum)
+        # get some random training images
 
-for epoch in range(2):
+        dataiter = iter(trainloader)
+        images, labels = dataiter.next()
 
-    running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
-        # get the inputs
-        inputs, labels = data
+        # show images
+        imshow(torchvision.utils.make_grid(images))
+        # print labels
+        print(' '.join('%5s' % classes[labels[j]] for j in range(1)))
 
-        # zero the parameter gradients
-        optimizer.zero_grad()
+        net = Net()
 
-        # forward + backward + optimize
-        outputs = net(inputs)
-        loss = criterion(outputs, labels)
-        loss.backward()
-        optimizer.step()
+        criterion = nn.CrossEntropyLoss()
 
-        # print statistics
-        running_loss += loss.item()
-        if i % 2000 == 1999:    # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / 2000))
+        if optimizer_type == 'sgd':
+            optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=momentum)
+        elif optimizer_type == 'rms_prop':
+            optimizer = optim.RMSprop(net.parameters(), lr=learning_rate, momentum=momentum)
+        elif optimizer_type == 'ada_delta':
+            optimizer = optim.Adadelta(net.parameters(), lr=learning_rate, rho=0.9, eps=1e-06, weight_decay=0)
+        else:
+            # default sgd
+            optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=momentum)
+
+        for epoch in range(2):
+
             running_loss = 0.0
+            for i, data in enumerate(trainloader, 0):
+                # get the inputs
+                inputs, labels = data
 
-print('Finished Training')
+                # zero the parameter gradients
+                optimizer.zero_grad()
 
-dataiter = iter(testloader)
-images, labels = dataiter.next()
+                # forward + backward + optimize
+                outputs = net(inputs)
+                loss = criterion(outputs, labels)
+                loss.backward()
+                optimizer.step()
 
-# print images
-imshow(torchvision.utils.make_grid(images))
-print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(1)))
+                # print statistics
+                running_loss += loss.item()
+                if i % 2000 == 1999:  # print every 2000 mini-batches
+                    print('[%d, %5d] loss: %.3f' %
+                          (epoch + 1, i + 1, running_loss / 2000))
+                    running_loss = 0.0
 
-outputs = net(images)
+        print('Finished Training')
 
-_, predicted = torch.max(outputs, 1)
+        dataiter = iter(testloader)
+        images, labels = dataiter.next()
 
-print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
-                              for j in range(1)))
+        # print images
+        imshow(torchvision.utils.make_grid(images))
+        print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(1)))
 
-correct = 0
-total = 0
-with torch.no_grad():
-    for data in testloader:
-        images, labels = data
         outputs = net(images)
-        _, predicted = torch.max(outputs.data, 1)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
 
-print('Accuracy of the network on the 10000 test images: %d %%' % (
-    100 * correct / total))
-
-class_correct = list(0. for i in range(10))
-class_total = list(0. for i in range(10))
-with torch.no_grad():
-    for data in testloader:
-        images, labels = data
-        outputs = net(images)
         _, predicted = torch.max(outputs, 1)
-        c = (predicted == labels).squeeze()
-        for i in range(1):
-            label = labels[i]
-            class_correct[label] += c[i].item()
-            class_total[label] += 1
 
+        print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
+                                      for j in range(1)))
 
-for i in range(10):
-    print('Accuracy of %5s : %2d %%' % (
-        classes[i], 100 * class_correct[i] / class_total[i]))
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for data in testloader:
+                images, labels = data
+                outputs = net(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
 
-dataiter._shutdown_workers()
+        print('Accuracy of the network on the 10000 test images: %d %%' % (
+                100 * correct / total))
 
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        class_correct = list(0. for i in range(10))
+        class_total = list(0. for i in range(10))
+        with torch.no_grad():
+            for data in testloader:
+                images, labels = data
+                outputs = net(images)
+                _, predicted = torch.max(outputs, 1)
+                c = (predicted == labels).squeeze()
+                for i in range(1):
+                    label = labels[i]
+                    class_correct[label] += c[i].item()
+                    class_total[label] += 1
 
+        for i in range(10):
+            print('Accuracy of %5s : %2d %%' % (
+                classes[i], 100 * class_correct[i] / class_total[i]))
 
-print(device)
+        dataiter._shutdown_workers()
 
+        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-
-########################################################################
-# The rest of this section assumes that `device` is a CUDA device.
-#
-# Then these methods will recursively go over all modules and convert their
-# parameters and buffers to CUDA tensors:
-#
-# .. code:: python
-#
-#     net.to(device)
-#
-#
-# Remember that you will have to send the inputs and targets at every step
-# to the GPU too:
-#
-# .. code:: python
-#
-#         inputs, labels = inputs.to(device), labels.to(device)
-#
-# Why dont I notice MASSIVE speedup compared to CPU? Because your network
-# is realllly small.
-#
-# **Exercise:** Try increasing the width of your network (argument 2 of
-# the first ``nn.Conv2d``, and argument 1 of the second ``nn.Conv2d`` –
-# they need to be the same number), see what kind of speedup you get.
-#
-# **Goals achieved**:
-#
-# - Understanding PyTorch's Tensor library and neural networks at a high level.
-# - Train a small neural network to classify images
-#
-# Training on multiple GPUs
-# -------------------------
-# If you want to see even more MASSIVE speedup using all of your GPUs,
-# please check out :doc:`data_parallel_tutorial`.
+        print(device)
